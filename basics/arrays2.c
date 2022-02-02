7aefa16a05a4f9e68eb8e007429a35b39cbe334d
Charlie Wiseman <charles.wiseman@bc.edu>
2022-02-02 19:23:24 +0000

    adding pointers examples

--- basics/arrays2.c
+++ basics/arrays2.c
@@ -3,17 +3,17 @@
 int main()
 {
     // declare and allocate space for an array of 5 ints
-    int nums[5];
+    char nums[5];
 
-    nums[0] = 100;
-    nums[1] = -143;
-    nums[2] = 0;
-    nums[3] = 123456;
-    nums[4] = -7;
+    nums[0] = 'a';
+    nums[1] = 'b';
+    nums[2] = 'c';
+    nums[3] = 'd';
+    nums[4] = 'e';
 
     for (int i=-10; i<10; ++i)
     {
-        printf("nums[%i] = %i\n", i, nums[i]);
+        printf("nums[%i] = %c\n", i, nums[i]);
     }
 
     return 0;
--- basics/strings.c
+++ basics/strings.c
@@ -5,6 +5,8 @@ int main()
 {
     char name[] = "Mario";
 
+    name[5] = 'M';
+
     int length = strlen(name);
 
     printf("%s: %i\n", name, length);
