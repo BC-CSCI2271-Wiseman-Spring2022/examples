819633b63c5dc3946725058d9526be92084b3b2a
Charlie Wiseman <charles.wiseman@bc.edu>
2022-03-16 01:58:13 +0000

    adding makefile examples

---
+++ makefiles/main2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "int_list.h"
+
+int main()
+{
+    node *head = NULL;
+
+    add_node(&head, 16);
+    add_node(&head, 8);
+    add_node(&head, 32);
+    add_node(&head, 4);
+
+    print_list(head);
+    free_list(&head);
+    return 0;
+}
